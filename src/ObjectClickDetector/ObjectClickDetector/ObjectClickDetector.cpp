02e5b6125e3444741cda44f3e1a38981d68ac61e
Inefan <107054911+Inefan@users.noreply.github.com>
2025-11-19 15:56:04 +0200

    Create model download, all work

--- src/ObjectClickDetector/ObjectClickDetector/ObjectClickDetector.cpp
+++ src/ObjectClickDetector/ObjectClickDetector/ObjectClickDetector.cpp
@@ -14,23 +14,17 @@ void onMouse(int event, int x, int y, int flags, void* userdata) {
         cout << "Click at (" << x << ", " << y << ")" << endl;
     }
 }
-
-int main() {
-    string imagePath = "C:\\Users\\MS\\OneDrive\\Desktop\\ObjectClickDetector\\test.jpg";
-    cv::Mat img = imread(imagePath);
-
-    if (img.empty()) {
-        cout << "Could not open or find the image!\n";
-        return -1;
+torch::jit::script::Module model;
+void load_model(const std::string& path) {
+    try {
+        model = torch::jit::load(path);
+        model.to(torch::kCPU);
+        model.eval();
+        std::cout << "Model loaded successfully!\n";
     }
-
-    namedWindow("Image", WINDOW_AUTOSIZE);
-    setMouseCallback("Image", onMouse);
-    imshow("Image", img);
-
-    cout << "Click on the image window.\n";
-    waitKey(0);
-    return 0;
+    catch (const c10::Error& e) {
+        std::cerr << "Error loading model: " << e.what() << "\n";
+    }
 }
 torch::Tensor preprocess_image(const cv::Mat& img_original) {
     const int H = 640;
@@ -52,6 +46,28 @@ torch::Tensor preprocess_image(const cv::Mat& img_original) {
     tensor = tensor.div(255.0);
 
     return tensor;
+}
+
+int main() {
+    string imagePath = "C:\\Users\\MS\\OneDrive\\Desktop\\ObjectClickDetector\\test.jpg";
+
+    load_model("C:\\Users\\MS\\OneDrive\\Desktop\\ObjectClickDetector\\model\\yolov5s.torchscript");
+    cv::Mat img = imread(imagePath);
+    if (img.empty()) {
+        cout << "Could not open or find the image!\n";
+        return -1;
+
+    }
+
+    namedWindow("Image", WINDOW_AUTOSIZE);
+    setMouseCallback("Image", onMouse);
+    imshow("Image", img);
+
+    cout << "Click on the image window.\n";
+    waitKey(0);
+    
+    return 0;
+
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
