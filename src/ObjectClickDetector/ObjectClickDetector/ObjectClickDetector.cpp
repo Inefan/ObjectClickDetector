a89c7ef23eb54765f8248dd936fae2416f287670
Inefan <107054911+Inefan@users.noreply.github.com>
2025-11-25 21:30:22 +0200

    Create struct DetectBox and update onMouse

--- src/ObjectClickDetector/ObjectClickDetector/ObjectClickDetector.cpp
+++ src/ObjectClickDetector/ObjectClickDetector/ObjectClickDetector.cpp
@@ -9,10 +9,50 @@
 using namespace cv;
 using namespace std;
 
-void onMouse(int event, int x, int y, int flags, void* userdata) {
-    if (event == EVENT_LBUTTONDOWN) {
-        cout << "Click at (" << x << ", " << y << ")" << endl;
+
+
+cv::Mat original_image;
+int selected_box = -1;
+struct DetectBox {
+    cv::Rect box;
+    int cls;
+    float score;
+};
+vector<DetectBox> detected_boxes;
+
+void onMouse(int event, int x, int y, int flags, void* userdata){
+    if (event != cv::EVENT_LBUTTONDOWN) {
+        return;
     }
+    cout << "Click at (" << x << ", " << y << ")" << endl;
+
+    selected_box = -1;
+
+    for (int i = 0; i < detected_boxes.size(); i++) {
+        const auto& b = detected_boxes[i].box;
+
+        int x1 = b.x;
+        int y1 = b.y;
+        int x2 = b.x + b.width;
+        int y2 = b.y + b.height;
+
+        if (x >= x1 && x <= x2 && y >= y1 && y <= y2) {
+            selected_box = i;
+            break;
+        }
+    }
+    if (selected_box == -1) {
+        cout << "Clicked outsite any box" << endl;
+        return; 
+    }
+    cv::Mat img = original_image.clone();
+    cv::rectangle(img, detected_boxes[selected_box].box, cv::Scalar(0, 255, 0), 2);
+
+    string label = "class" + std::to_string(detected_boxes[selected_box].cls);
+    cv::putText(img, label, cv::Point(x + 5, y - 5),
+                cv::FONT_HERSHEY_SIMPLEX, 0.6, cv::Scalar(0, 255, 0), 2);
+    cv::imshow("Image", img);
+
 }
 torch::jit::script::Module model;
 void load_model(const std::string& path) {
